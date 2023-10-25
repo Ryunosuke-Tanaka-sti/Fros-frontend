c43c0b7294b8a540386b087073824a0474db32d6
田中龍之介 <ry-tanaka@sios.com>
2023-10-25 17:17:16 +0900

    feature:test push

--- src/constants/authConfig.ts
+++ src/constants/authConfig.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_APIKEY,
+  apiKey: 'test_12344545555',
   authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
   projectId: import.meta.env.VITE_FB_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
